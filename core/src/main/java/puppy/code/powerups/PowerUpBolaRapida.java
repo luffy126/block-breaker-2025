6ea3fc1c7d53bb590c87fb40b2c2ffa360745686
Pachiky <fraan16poncee@gmail.com>
2025-10-29 15:44:14 -0300

    Creación de mi branch. Implementacion de la clase PowerUpBolaRapida

--- core/src/main/java/puppy/code/powerups/PowerUpBolaRapida.java
+++ core/src/main/java/puppy/code/powerups/PowerUpBolaRapida.java
@@ -1,9 +1,13 @@
 package puppy.code.powerups;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.Timer;
 import puppy.code.game.BlockBreakerGame;
 
 public class PowerUpBolaRapida extends PowerUp {
+    private static final float MULTIPLICADOR_VELOCIDAD = 2.0f;
+    private static final float DURACION_MINIMA = 10f;
+    private static final float DURACION_MAXIMA = 15f;
 
     public PowerUpBolaRapida(int x, int y) {
         super(x, y, Color.YELLOW);
@@ -11,6 +15,16 @@ public class PowerUpBolaRapida extends PowerUp {
 
     @Override
     public void aplicarEfecto(BlockBreakerGame game) {
-        game.acelerarBola(1.5f);
+        game.acelerarBola(MULTIPLICADOR_VELOCIDAD);
+
+        float duracion = DURACION_MINIMA + (float)(Math.random() * (DURACION_MAXIMA - DURACION_MINIMA));
+
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run(){
+                game.restaurarVelocidadBola();
+            }
+        }, duracion);
     }
 }
+
